e501d141f0c0428457c8304d3acbd051076aafbe
ncdm-stldr <ncdm.stldr@gmail.com>
2019-05-11 18:55:16 +0200

    removing function getColorFor and adding toString method to class Color. Type of refactoring applied: Move function. Concerned bad smell: Shotgun Surgery

--- src/main/java/heig_vd/labo4/Color.java
+++ src/main/java/heig_vd/labo4/Color.java
@@ -4,11 +4,26 @@ public class Color {
 
     int color;
 
-    public Color(int color){
+    public Color(int color) {
         this.color = color;
     }
 
-    public int colorToInt(){
+    public int colorToInt() {
         return color;
     }
-}
+
+    @Override
+    public String toString() {
+            switch (color) {
+                case 1:
+                    return "blue";
+                case 2:
+                    return "red";
+                case 3:
+                    return "yellow";
+                default:
+                    return "no color";
+            }
+        }
+
+    }
