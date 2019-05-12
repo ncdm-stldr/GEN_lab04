1e0f67ea38305bf76cdaa6799a370799e2944a4a
ncdm-stldr <ncdm.stldr@gmail.com>
2019-05-12 17:08:50 +0200

    removing function getSizeFor and adding toString method to class Size. Type of refactoring applied: Move function. Concerned bad smell: Shotgun Surgery

--- src/main/java/heig_vd/labo4/Size.java
+++ src/main/java/heig_vd/labo4/Size.java
@@ -11,4 +11,23 @@ public class Size {
     public int sizeToInt(){
         return size;
     }
+
+    public String toString(){
+        switch (size) {
+            case 1:
+                return "XS";
+            case 2:
+                return "S";
+            case 3:
+                return "M";
+            case 4:
+                return "L";
+            case 5:
+                return "XL";
+            case 6:
+                return "XXL";
+            default:
+                return "Invalid Size";
+        }
+    }
 }
